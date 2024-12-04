dd5c847ba7d3294f7db5435d955765fac4ca3670
Gonzalo Medina <gonzalo@bigcartel.com>
2024-12-04 16:52:02 -0700

    fix: update button label

--- components/locations/Sidebar.tsx
+++ components/locations/Sidebar.tsx
@@ -70,7 +70,7 @@ const Sidebar = () => {
 
   return (
     <Button variant='secondary' isFullWidth onClick={handleClick}>
-      View the Documentation
+      View Documentation
     </Button>
   );
 };
